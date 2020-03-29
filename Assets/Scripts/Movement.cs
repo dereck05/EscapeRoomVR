5d8cdf246bbd4a4f365850be085704da1e2b8972
Abraham Meza Vega <abrahammv_6@hotmail.com>
2020-03-28 22:58:53 -0600

    [New] Movement system

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour{
+
+    public float velocity = 0.2f;
+
+    private float verticalDisplacement = 0.0f;
+    private float HorizontalDisplacement = 0.0f;
+
+    void Start(){}
+
+    void Update(){
+        verticalDisplacement = Input.GetAxis("Vertical") * velocity;
+        HorizontalDisplacement = Input.GetAxis("Horizontal") * velocity;
+    }
+
+    void FixedUpdate(){
+        move();
+    }
+
+    private void move(){
+        Vector3 direction = (Vector3.forward * verticalDisplacement) + (Vector3.right * HorizontalDisplacement);
+        transform.position += transform.rotation * direction;
+    }
+}
---
+++ Assets/Scripts/View.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class View : MonoBehaviour{
+
+    public float sensitivity = 125.0f;
+
+    private float verticalRotation = 0.0f;
+    private float horizontalRotation = 0.0f;
+
+    void Start(){}
+
+
+    void Update(){
+        verticalRotation = -Input.GetAxis("Mouse Y") * sensitivity;
+        horizontalRotation = Input.GetAxis("Mouse X") * sensitivity;
+    }
+
+    void FixedUpdate(){
+        transform.Rotate(new Vector3(0, horizontalRotation * Time.deltaTime, 0));
+        transform.GetChild(0).transform.Rotate(new Vector3(verticalRotation * Time.deltaTime, 0, 0));
+    }
+
+}
